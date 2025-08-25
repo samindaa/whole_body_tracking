bb02ac1ecbde9f01d47fced98c577ab14d504acd
Saminda Abeyruwan <samindaa@gamil.com>
2025-08-24 22:34:06 -0700

    small changes

--- scripts/rsl_rl/eval.py
+++ scripts/rsl_rl/eval.py
@@ -12,11 +12,11 @@ import cli_args  # isort: skip
 
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Train an RL agent with RSL-RL.")
-parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
-parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
-parser.add_argument(
-    "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
-)
+# parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
+# parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
+# parser.add_argument(
+#     "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
+# )
 parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
 parser.add_argument("--motion_file", type=str, default=None, help="Path to the motion file.")
@@ -26,15 +26,15 @@ cli_args.add_rsl_rl_args(parser)
 AppLauncher.add_app_launcher_args(parser)
 args_cli, hydra_args = parser.parse_known_args()
 # always enable cameras to record video
-if args_cli.video:
-    args_cli.enable_cameras = True
+# if args_cli.video:
+#     args_cli.enable_cameras = True
 
 # clear out sys.argv for Hydra
 sys.argv = [sys.argv[0]] + hydra_args
 
 # launch omniverse app
 app_launcher = AppLauncher(args_cli)
-simulation_app = app_launcher.app
+# simulation_app = app_launcher.app
 
 """Rest everything follows."""
 
@@ -46,11 +46,11 @@ import torch
 from rsl_rl.runners import OnPolicyRunner
 
 from isaaclab.envs import (
-    DirectMARLEnv,
-    DirectMARLEnvCfg,
-    DirectRLEnvCfg,
+    # DirectMARLEnv,
+    # DirectMARLEnvCfg,
+    # DirectRLEnvCfg,
     ManagerBasedRLEnvCfg,
-    multi_agent_to_single_agent,
+    # multi_agent_to_single_agent,
 )
 from isaaclab.utils.dict import print_dict
 from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlVecEnvWrapper
@@ -63,7 +63,7 @@ from whole_body_tracking.utils.exporter import attach_onnx_metadata, export_moti
 
 
 @hydra_task_config(args_cli.task, "rsl_rl_cfg_entry_point")
-def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agent_cfg: RslRlOnPolicyRunnerCfg):
+def main(env_cfg: ManagerBasedRLEnvCfg, agent_cfg: RslRlOnPolicyRunnerCfg):
     """Play with RSL-RL agent."""
     agent_cfg: RslRlOnPolicyRunnerCfg = cli_args.parse_rsl_rl_cfg(args_cli.task, args_cli)
     env_cfg.scene.num_envs = args_cli.num_envs if args_cli.num_envs is not None else env_cfg.scene.num_envs
@@ -108,32 +108,28 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     else:
         print(f"[INFO] Loading experiment from directory: {log_root_path}")
         resume_path = get_checkpoint_path(log_root_path, agent_cfg.load_run, agent_cfg.load_checkpoint)
-    print(f"[INFO]: Loading model checkpoint from: {resume_path}")
-
-    # loaded_dict = torch.load(resume_path, weights_only=False)
-    # print("XXXXX", loaded_dict["model_state_dict"].keys())
-    # raise ValueError
+        print(f"[INFO]: Loading model checkpoint from: {resume_path}")
 
     # create isaac environment
-    env = gym.make(args_cli.task, cfg=env_cfg, render_mode="rgb_array" if args_cli.video else None)
+    env = gym.make(args_cli.task, cfg=env_cfg, render_mode=None)
 
-    log_dir = os.path.dirname(resume_path)
+    # log_dir = os.path.dirname(resume_path)
 
     # wrap for video recording
-    if args_cli.video:
-        video_kwargs = {
-            "video_folder": os.path.join(log_dir, "videos", "play"),
-            "step_trigger": lambda step: step == 0,
-            "video_length": args_cli.video_length,
-            "disable_logger": True,
-        }
-        print("[INFO] Recording videos during training.")
-        print_dict(video_kwargs, nesting=4)
-        env = gym.wrappers.RecordVideo(env, **video_kwargs)
+    # if args_cli.video:
+    #     video_kwargs = {
+    #         "video_folder": os.path.join(log_dir, "videos", "play"),
+    #         "step_trigger": lambda step: step == 0,
+    #         "video_length": args_cli.video_length,
+    #         "disable_logger": True,
+    #     }
+    #     print("[INFO] Recording videos during training.")
+    #     print_dict(video_kwargs, nesting=4)
+    #     env = gym.wrappers.RecordVideo(env, **video_kwargs)
 
     # convert to single-agent instance if required by the RL algorithm
-    if isinstance(env.unwrapped, DirectMARLEnv):
-        env = multi_agent_to_single_agent(env)
+    # if isinstance(env.unwrapped, DirectMARLEnv):
+    #     env = multi_agent_to_single_agent(env)
 
     # wrap around environment for rsl-rl
     env = RslRlVecEnvWrapper(env)
@@ -158,20 +154,21 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     attach_onnx_metadata(env.unwrapped, args_cli.wandb_path if args_cli.wandb_path else "none", export_model_dir)
     # reset environment
     obs, _ = env.get_observations()
+    print("XXX", obs.shape)
     timestep = 0
     # simulate environment
-    while simulation_app.is_running():
-        # run everything in inference mode
-        with torch.inference_mode():
-            # agent stepping
-            actions = policy(obs)
-            # env stepping
-            obs, _, _, _ = env.step(actions)
-        if args_cli.video:
-            timestep += 1
-            # Exit the play loop after recording one video
-            if timestep == args_cli.video_length:
-                break
+    # while simulation_app.is_running():
+    #     # run everything in inference mode
+    #     with torch.inference_mode():
+    #         # agent stepping
+    #         actions = policy(obs)
+    #         # env stepping
+    #         obs, _, _, _ = env.step(actions)
+    #     if args_cli.video:
+    #         timestep += 1
+    #         # Exit the play loop after recording one video
+    #         if timestep == args_cli.video_length:
+    #             break
 
     # close the simulator
     env.close()
@@ -181,4 +178,4 @@ if __name__ == "__main__":
     # run the main function
     main()
     # close sim app
-    simulation_app.close()
+    # simulation_app.close()
